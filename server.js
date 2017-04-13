2fc5edffcd54d6af6a9a5f267bfefb29b35d09f9
Kai Chen <chenkaiser@gmail.com>
2017-04-13 14:36:16 -0400

    static html, css, js support

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/html',express.static(path.join(__dirname,'static/html')));
+app.use('/css',express.static(path.join(__dirname,'static/css')));
+app.use('/js',express.static(path.join(__dirname,'static/js')));
 
 
 // Initialize the app.
@@ -15,12 +19,14 @@ var server = app.listen(process.env.PORT || 8888, function () {
 // ROUTES
 var router = express.Router();
 
+//http://localhost:8888/api/test
 router.get('/test', function(req,res){
   res.json({message: 'Hi, highspeed!'});
 });
 
 app.use('/api', router);
 
+//http://localhost:8888/test
 app.get('/test',function(req,res){
     res.send('Hello highspeed');
 })
